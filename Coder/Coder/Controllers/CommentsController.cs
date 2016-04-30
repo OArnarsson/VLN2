6879b89441792edb26042bf9146c6a2cfc851a68
Gunnar Torfi <gunnartorfis@gmail.com>
2016-04-30 22:08:24 +0000

    CommentController and getting all comments

---
+++ Coder/Coder/Controllers/CommentsController.cs
@@ -0,0 +1,45 @@
+﻿using Coder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Coder.Models.Entity;
+using Coder.Repository;
+
+namespace Coder.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private CommentsRepository commentsRepo = new CommentsRepository();
+
+        // GET: Comments
+        public ActionResult Index(int taskId)
+        {
+            return View(commentsRepo.getAllComments(taskId));
+        }
+
+        // POST: Comments/Create
+        [HttpPost]
+        public ActionResult Create(Comment comment)
+        {
+            return View();
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Coder/Coder/Repository/CommentsRepository.cs
@@ -0,0 +1,23 @@
+﻿using Coder.Models;
+using Coder.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coder.Repository
+{
+    public class CommentsRepository
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IEnumerable<Comment> getAllComments(int taskId)
+        {
+            return from comment in db.Comments.ToList()
+                   where comment.ProjectTaskId == taskId
+                   select comment;
+        }
+
+
+    }
+}
