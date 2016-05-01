334b12ffd8ad1fbec428fc9877e9ead69ea525a5
Þorgrímur Jónasarson <toggi737@gmail.com>
2016-05-01 13:32:35 +0000

    Added gulpfile and SASS

---
+++ Coder/Coder/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync');
+
+const $ = require('gulp-load-plugins')();
+const reload = browserSync.reload;
+
+gulp.task('styles', () => {
+  return gulp.src('Content/sass/theme.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass.sync({
+        outputStyle: 'expanded',
+        precision: 10,
+        includePaths: ['.']
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+    .pipe($.sourcemaps.write())
+    //.pipe(plugins.minifyCss(options.minifyCss))
+    .pipe(gulp.dest('Content'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('watcher', ['styles'], () => {
+    gulp.watch('Content/sass/*.scss', ['styles']);
+});
+
+gulp.task('browsersync', ['styles'], () => {
+    browserSync.init({
+        open: 'external',
+        host: 'localhost:3980',
+        proxy: 'localhost:3980',
+        open: false,
+        notify: false
+    });
+    gulp.start('watcher');
+});
+
+gulp.task('default', ['watcher'], () => {
+});
