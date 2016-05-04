88bf37b3a8ff6e2ed37f23221a3eb0b7426d0a6f
andriandresson <andriandresson@gmail.com>
2016-05-04 23:41:30 +0000

    added working countdown in Home/Index

---
+++ Coder/Coder/Scripts/CountDown.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function () {
+    
+    $('.TimeLeft').each(function (i, selected) {
+        //to see the timeFormat of the value:  
+        TheClock(selected.id, $(selected).val());
+    });
+      
+    function TheClock(id, val) {
+        var currentDate = new Date();
+        var futureDate = new Date(val);
+        var difference = (futureDate.getTime() / 1000) - (currentDate.getTime() / 1000);
+
+        if (difference > 0) {
+            var clock = $("." + id).FlipClock(difference, {
+                clockFace: 'DailyCounter',
+                countdown: true,
+                showSeconds: true
+            });
+        }
+        else {
+            $("#rem"+ id).hide();
+        }
+    }
+});
+
+
+	
