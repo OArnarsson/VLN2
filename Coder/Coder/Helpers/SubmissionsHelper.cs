7cb89480487140b90ba900bec2b4374b005df321
Þorgrímur Jónasarson <toggi737@gmail.com>
2016-05-08 23:42:33 +0000

    Added base for submissionshelper

---
+++ Coder/Coder/Helpers/SubmissionsHelper.cs
@@ -0,0 +1,22 @@
+﻿using Coder.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Coder.Helpers
+{
+    public class SubmissionsHelper
+    {
+        public bool createSubmission(ProjectTask task)
+        {
+            Debug.AutoFlush = true;
+
+            var workingFolder = System.IO.Directory.GetCurrentDirectory();
+            string projectRoot = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            return false;
+        }
+    }
+}
