df76c07e37b12905c586e8b438f4c3b78aeaae5e
elisabet15 <steinark15@ru.is>
2016-05-08 17:14:18 +0000

    Basic error handling added, not complete, IIS errors todo

---
+++ Coder/Coder/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Coder.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ViewResult Index()
+        {
+            return View("Error");
+        }
+
+        public ViewResult BadRequest()
+        {
+            return View("BadRequest");
+        }
+
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+    }
+}
