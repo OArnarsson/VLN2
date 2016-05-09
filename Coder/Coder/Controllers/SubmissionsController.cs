8afa49c69eb5c7e4dedf3a868302d0adadd99b50
Þorgrímur Jónasarson <toggi737@gmail.com>
2016-05-09 15:44:44 +0000

    Resolved conflict and started working on submission details

--- Coder/Coder/Controllers/SubmissionsController.cs
+++ Coder/Coder/Controllers/SubmissionsController.cs
@@ -51,7 +51,7 @@ namespace Coder.Controllers
                 throw new HttpException((int)HttpStatusCode.NotFound, "Not found!");
             }
 
-            return View();
+            return View(submission);
         }
     }
 }
