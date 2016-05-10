de45451c48f299adf65f3de6a09a7c5178c47525
OArnarsson <orriarnars@gmail.com>
2016-05-10 17:19:14 +0000

    UserCreate & Password Length
    Styled UserCreate view.
    Set PW min length back to 4.

--- Coder/Coder/Models/ViewModels/CreateUserViewModel.cs
+++ Coder/Coder/Models/ViewModels/CreateUserViewModel.cs
@@ -9,13 +9,13 @@ namespace Coder.Models.ViewModels
     public class CreateUserViewModel : UserViewModel
     {
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
