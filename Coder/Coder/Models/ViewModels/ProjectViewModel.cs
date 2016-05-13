ad21238271f75ead9e8b2cdc46fda06d238b7b09
andriandresson <andriandresson@gmail.com>
2016-05-13 14:15:46 +0000

    NewViewModel + working Value in Home Controller

---
+++ Coder/Coder/Models/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Coder.Models.Entity;
+using Foolproof;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Coder.Models.ViewModels
+{
+    public class ProjectViewModel
+    {
+       public Project project { get; set; }
+
+        public double value { get; set; }
+    }
+}
